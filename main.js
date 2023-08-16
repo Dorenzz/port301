9341113f53929fd3b17406b02e2a2ffb3edb6daa
Dorenzz <142439614+Dorenzz@users.noreply.github.com>
2023-08-17 02:24:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("mobile-active");
+});
